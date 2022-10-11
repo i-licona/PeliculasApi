9a58eb4aceff13f8a26b74adc9554d1f8a8c4a12
Jose Ignacio Licona <jos.licona11@gmail.com>
2022-10-11 11:40:12 -0500

    first commit

---
+++ startup.cs
@@ -0,0 +1,39 @@
+using Microsoft.OpenApi.Models;
+
+namespace PeliculasApi
+{
+  public class Startup
+  {
+    private readonly IConfiguration configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+      this.configuration = configuration;
+    }
+
+    /* Configure services method */
+    public void ConfigureServices(IServiceCollection services){
+      services.AddControllers();
+      services.AddEndpointsApiExplorer();
+      services.AddSwaggerGen( s => {
+        s.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Peliculas", Version = "v1"});
+      });
+    }
+
+    /* Configure method */
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env){
+      if (env.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseRouting();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints => {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
